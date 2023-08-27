db5354696912e6c37d8520723e1a72ac96b4ddc9
kroryan <69607760+kroryan@users.noreply.github.com>
2023-08-27 13:07:35 +0100

    Update chat-vicinstall.sh

--- chat-vicinstall.sh
+++ chat-vicinstall.sh
@@ -1,8 +1,6 @@
-chmod +x chat-vic7.sh
+          chmod +x chat-vic7.sh
           cd /$PREFIX/bin
           echo 'clear' > chat-v
-          echo 'cd' >> chat-v
-          echo 'cd llama.cpp' >> chat-v
-          echo 'cd examples' >> chat-v
+          echo 'cd //data/data/com.termux/files/home/llama' >> chat-v
           echo './chat-vic7.sh' >> chat-v
           chmod +x /$PREFIX/bin/chat-v
