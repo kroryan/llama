a803607ea08b57f2f37383d4d85cb492182dc6d7
kroryan <69607760+kroryan@users.noreply.github.com>
2023-08-27 04:20:13 +0100

    Update chat-vic7B.sh

--- chat-vic7B.sh
+++ chat-vic7B.sh
@@ -13,4 +13,4 @@ cd ..
 #
 ./main -m ./models/vicuna-7b-v1.5.ggmlv3.q2_K.bin -c 512 -b 1024 -n 256 --keep 48 \
     --repeat_penalty 1.0 --color -i \
-    -r "User:" -f prompts/chat-with-bob.txt
+    -r "User:" -f //data/data/com.termux/files/home/llama/prompts/chat-with-bob.txt
